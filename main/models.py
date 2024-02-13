bb49fcbb209bfaaeb006fa2068dc8d50e86fe3eb
MrBlueSky2001 <smatisa951@iesmartinezm.es>
2024-02-13 13:51:47 +0100

    Models Hechos

--- main/models.py
+++ main/models.py
@@ -1,8 +1,20 @@
 # Create your models here.
-class Post():
+from django.db import models
+from django.contrib.auth.models import User
+class Post(models.Model):
+    titulo = models.CharField(max_length=100)
+    cuerpo = models.TextField(help_text="Incluye el contenido de tu articulo")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.titulo} {self.user.username}'
+
+    class Meta:
+        verbose_name_plural = "Posts"
+
     pass
 
-class Valoracion():
+class Valoracion(models.Model):
     RATE_CHOICES = [
         (1,1),
         (2, 2),
@@ -10,3 +22,13 @@ class Valoracion():
         (4, 4),
         (5, 5),
     ]
+    rating = models.PositiveIntegerField(choices=RATE_CHOICES)
+    comment = models.TextField()
+    fecha_registro = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.rating} {self.comment} {self.fecha_registro} {self.user.username}'
+
+    class Meta:
+        verbose_name_plural = "Valoraciones"
