faa04825d13e7eba30b34be55bb729ace9f2fb6e
MrBlueSky2001 <smatisa951@iesmartinezm.es>
2024-02-13 14:32:24 +0100

    Varias cosas 'hechas' Parte 2

--- main/views.py
+++ main/views.py
@@ -1,6 +1,14 @@
 # Create your views here.
+from rest_framework import permissions
+from main.serializers import PostSerializer, ValoracionSerializer
+from main.models import Post, Valoracion
+
 class PostViewSet():
-    pass
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class ValoracionViewSet():
-    pass
+    queryset = Valoracion.objects.all()
+    serializer_class = ValoracionSerializer
+    permission_classes = [permissions.IsAuthenticated]
