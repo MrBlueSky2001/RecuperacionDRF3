6206206011261291513ece8df6f90e88a62846c3
MrBlueSky2001 <smatisa951@iesmartinezm.es>
2024-02-13 14:43:31 +0100

    Varias cosas 'hechas' Parte 3

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
+from main import views
 
 
 router = routers.DefaultRouter()
+router.register(r'post', views.PostViewSet)
+router.register(r'valoracion', views.ValoracionViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
+urlpatterns += router.urls
--- main/views.py
+++ main/views.py
@@ -1,14 +1,14 @@
 # Create your views here.
-from rest_framework import permissions
+from rest_framework import permissions, viewsets
 from main.serializers import PostSerializer, ValoracionSerializer
 from main.models import Post, Valoracion
 
-class PostViewSet():
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-class ValoracionViewSet():
+class ValoracionViewSet(viewsets.ModelViewSet):
     queryset = Valoracion.objects.all()
     serializer_class = ValoracionSerializer
     permission_classes = [permissions.IsAuthenticated]
