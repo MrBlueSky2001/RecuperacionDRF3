28c70a20a5850b1d742d6f60141dacb96d24ad92
MrBlueSky2001 <smatisa951@iesmartinezm.es>
2024-05-28 12:40:29 +0200

    RecHecho1

--- main/views.py
+++ main/views.py
@@ -16,4 +16,4 @@ class ValoracionViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     ordering_fields = ['fecha_registro']
 
-#Prueba de push
+#Prueba de push1
